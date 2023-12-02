365f46fac1c7bdbd1c84ec1dd4a5a9c8087db2f0
rdhaulian <151128727+radhiaaulia16@users.noreply.github.com>
2023-12-02 23:06:59 +0700

    Add files via upload

---
+++ Jobsheet 1- Algoritma, Pemograman IDE/Tugas_1_Jobsheet_1.cpp
@@ -0,0 +1,12 @@
+//created by Radhia Aulia Nisa 23343049
+
+#include <stdio.h> //header yang berisikan library
+
+int main(){ //tempat program dieksekusi
+	
+	//menampilkan output Hello World
+	printf("Hello world");
+	
+	//membalikkan nilai 0
+	return 0;
+}
